e717712e7b54131c8cd69629c40a75658cd08e8f
David Einenkel <david.einenkel@gmail.com>
2023-04-04 19:44:17 +0200

    Dashboard nav

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,8 +1,21 @@
 async function redirectIfNotAuthenticated() {
     const isAuthenticated = await isLoggedIn();
     if (!isAuthenticated) {
-        window.location.href = "./login.html";
+        //window.location.href = "./login.html";
     }
 }
 
 redirectIfNotAuthenticated();
+
+const navEl = document.querySelectorAll(".nav-element");
+
+function activeLink() {
+    navEl.forEach((item) => item.classList.remove("active"));
+    this.classList.add("active");
+}
+navEl.forEach((item) => item.addEventListener("click", activeLink));
+
+function logout() {
+    localStorage.removeItem("jwtToken");
+    window.location.href = "./";
+}
